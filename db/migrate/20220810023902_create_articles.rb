08536fd42ae5479c5403de162a6ba92bbfd01dbd
Kei <kei.n140802@gmail.com>
2022-08-10 11:51:24 +0900

    マイグレーションファイルを実行、articlesテーブルを作成

--- db/migrate/20220810023902_create_articles.rb
+++ db/migrate/20220810023902_create_articles.rb
@@ -1,9 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :image
-      t.integer :status
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end
