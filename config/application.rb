19c0efe205427d70fdf27fa655bf0cf12e9985a4
Kei <kei.n140802@gmail.com>
2022-08-10 11:25:22 +0900

    タイムゾーンを設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module ShareHouseworkApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.assets false
