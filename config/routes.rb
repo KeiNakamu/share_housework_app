3c819505dca138e5186b6ef9aa2934f93a7e0ead
Kei <kei.n140802@gmail.com>
2022-08-11 22:31:58 +0900

    :sparkles: deviceのメール設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :articles do
     resources :procedures
   end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
