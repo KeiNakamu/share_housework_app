3324fd4f9347b074232123d2585d8bec3bf2977f
Kei <kei.n140802@gmail.com>
2022-08-12 11:21:19 +0900

    :art: artikleとcommentのアソシエーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_134032) do
+ActiveRecord::Schema.define(version: 2022_08_12_021501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2022_08_11_134032) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "procedures", force: :cascade do |t|
@@ -54,5 +62,6 @@ ActiveRecord::Schema.define(version: 2022_08_11_134032) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "articles"
   add_foreign_key "procedures", "articles"
 end
