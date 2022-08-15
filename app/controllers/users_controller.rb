c13730332aac81b86433c907788c7ddc8dfae7a7
Kei <kei.n140802@gmail.com>
2022-08-15 14:13:28 +0900

    :art: ユーザー名にリンクを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to user_path(current_user.id) unless @user == current_user
+    # redirect_to user_path(current_user.id) unless @user == current_user
   end
 
   def edit
