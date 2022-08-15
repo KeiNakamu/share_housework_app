02a32723b395ce46d5aa0f2b3a73270cd06eb676
Kei <kei.n140802@gmail.com>
2022-08-15 19:13:45 +0900

    :art: userモデルにアップローダーを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  mount_uploader :image, ImageUploader
 
   def liked_by?(article_id)
     likes.where(article_id: article_id).exists?
