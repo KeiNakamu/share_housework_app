c44b9717ffdef83cdf10612b0ec3781e235ee9d2
Kei <kei.n140802@gmail.com>
2022-08-15 17:56:16 +0900

    :art: usersコントローラーのedit,updateアクションの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "ユーザーの内容を更新しました"
+    else
+      flash.now[:danger] = "ユーザーを更新できませんでした"
+      render :edit
+    end
   end
 
   private
