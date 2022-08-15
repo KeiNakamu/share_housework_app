8349402da6e5f57af583f4545e73c7a4d808b74a
Kei <kei.n140802@gmail.com>
2022-08-15 11:30:27 +0900

    :art: active_relationships と passive_relationships でアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_articles, through: :likes, source: :article
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
 
   def liked_by?(article_id)
     likes.where(article_id: article_id).exists?
