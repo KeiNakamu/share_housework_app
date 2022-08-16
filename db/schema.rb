5c244bbaad164abbd2b0f02858ca96503274f7f3
Kei <kei.n140802@gmail.com>
2022-08-17 00:06:15 +0900

    :sparkles: マイグレーションファイルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_145940) do
+ActiveRecord::Schema.define(version: 2022_08_16_150503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,6 @@ ActiveRecord::Schema.define(version: 2022_08_16_145940) do
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deadline"
-    t.datetime "date"
     t.index ["article_id"], name: "index_procedures_on_article_id"
   end
 
