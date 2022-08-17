fb0dd9a9e28823c8170612c9fc40709717f2c16d
Kei <kei.n140802@gmail.com>
2022-08-17 22:51:26 +0900

    :art: モデルにアソシエーションを定義

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,8 @@ class Article < ApplicationRecord
   has_many :liked_users, through: :likes, source: :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  has_many :articlecategories, dependent: :destroy
+  has_many :categories, through: :categories
   
   mount_uploader :image, ImageUploader
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :articlecategories, dependent: :destroy
+  has_many :articles, through: :categories
 end
