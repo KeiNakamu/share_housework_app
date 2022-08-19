24eafea8966f57e81da184c6fd0f4809c52fa1e0
Kei <kei.n140802@gmail.com>
2022-08-20 01:07:33 +0900

    :wrench: ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :categories
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :users do
     member do
       get :followings, :followers
