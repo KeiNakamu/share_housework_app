fa56c6da70891e959c634447c42117b6df07ae0a
Kei <kei.n140802@gmail.com>
2022-08-20 17:38:07 +0900

    :sparkles: コントローラーを編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,11 +6,9 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(article_id: params[:article_id])
-    flash.now[:notice] = "#{favorite.article.user.name}さんの投稿した記事をお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    flash[:notice] = "#{favorite.article.user.name}さんの投稿した記事をお気に入り解除しました"
   end
 end
