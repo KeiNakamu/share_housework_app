f5a4a016442800b0d138c940efc3243011d73956
Kei <kei.n140802@gmail.com>
2022-08-22 23:43:05 +0900

    :art: モデルに定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.where(status: :public).page(params[:page]).per(5)
     @article_search = @articles.ransack(params[:q])
-    @articles = @article_search.result.page(params[:page]).per(5)
+    @articles = @article_search.result.order(created_at: "DESC").page(params[:page]).per(5)
     @articles = @articles.where(article_categories: ArticleCategory.where(category_id: params[:q][:category_ids])).page(params[:page]).per(5) if params[:q].present? && params[:q][:category_ids].present?
   end
 
