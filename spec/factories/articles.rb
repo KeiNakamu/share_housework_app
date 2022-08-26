8689b39933643328cfad9e63c51c6d9203801269
Kei <kei.n140802@gmail.com>
2022-08-27 03:55:13 +0900

    :art: system/article_specのテストの実装完了

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,8 +2,17 @@
 FactoryBot.define do
   factory :article do
     title { 'タイトル' }
-    status { 'public' }
     association :user
+  end
+
+  factory :second_article, class: Article do
+    title { 'second_title' }
+    association :user
+  end
+
+  factory :third_article, class: Article do
+    title { '3rd_title' }
+    association :second_user
   end
 
   factory :article_private, class: Article do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,11 @@ FactoryBot.define do
     password { "password" }
     password_confirmation { "password" }
   end
+
+  factory :second_user, class: User do
+    name { "中村" }
+    email { "second@example2.com" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
+  end
 end
